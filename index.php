0b5696f61a428b9b9b86c7c52df83a2936be48b0
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2019-01-21 21:54:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
 <body>
 <img src="activity.jpg">
 <p>Activity Diagram</p>
+<p>
+This diagram shows the flow of process of this proposed app(Tutor finder app).There are two types of users here, the tutor who renders the service and the client who finds tutor & which the service is rendered to. 
+First of all system would check if you have an account,you can login if you have an existing account and if you dont have you wont be able to proceed further.For new user they must first create an account. Choose if either you are a client or a tutor. Then you can proceed to the next step. Each user has a separate and individual homepage with regards of what kind of user you are.For the tutor, there are various things he/she can do.Select on the categories of subject which he/she can specialized. Have an access to the list of clients and can either confirm an appointment or cancel.
+View ratings from clients, with these it can gain more client the higher rate he/she gets. Edit profile, he/she can edit one's information.On clients side, same as the tutors they can also edit and update their profile.Clients can choose directly tutors available within your location, or choose subjects first before proceeding who you want to make an appoinment to. An information of the tutor is provided as references for your needs.
+Clients are the one who are responsible of tutor's rating, the more competence the tutor is the higher rate they can give.It is up to the client which tutor they can choose. And this serves as an aid for the people who needs an educational assistance and opportunity for the people who wants sideline job in this field.
+</p>
 </body>
 </html>
